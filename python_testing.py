1376dbc1bb608846f49c467b62027c82da96e059
nchimitt <nchimitt@gmail.com>
2017-12-29 12:44:47 -0500

    Eliminate Hello, Hi api keys
    Added lines using functionality from other repository which is the python winner of the Binance competition for API usage. Testing getting prices (api keys are excluded, use your own!)

--- python_testing.py
+++ python_testing.py
@@ -1,1 +1,5 @@
-print("Hello, world!")
+from binance.client import Client
+api_key = #enter your key
+api_secret = #still enter your key
+client = Client(api_key, api_secret)
+prices = client.get_all_tickers()
